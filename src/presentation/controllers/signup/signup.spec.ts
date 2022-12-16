0a00d85529bb16b3bcb5a73f7ad49a135850abab
Raphael Rego <raphael.mlrego@gmail.com>
2022-12-16 17:12:54 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -137,7 +137,7 @@ describe('SignUp Controller', () => {
     const httpRequest = {
       body: {
         name: 'any_name',
-        email: 'invalid_email',
+        email: 'invalid_email@mail.com',
         password: 'any_password',
         passwordConfirmation: 'any_password'
       }
--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,11 +1,30 @@
+import validator from 'validator'
 import { EmailValidatorAdapter } from './email-validator'
+
+jest.mock('validator', () => {
+  const isEmail = (): boolean => {
+    return true
+  }
+
+  return { isEmail }
+})
 
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
 
-    const isValid = sut.isValid('invalid_email')
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+
+    const isValid = sut.isValid('invalid_email@mail.com')
 
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+
+    const isValid = sut.isValid('valid_email@mail.com')
+
+    expect(isValid).toBe(true)
   })
 })
