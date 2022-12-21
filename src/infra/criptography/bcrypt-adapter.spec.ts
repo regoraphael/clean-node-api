ad22157ac76d0f503e32b33714e5e4dfffee4e9b
Raphael Rego <raphael.mlrego@gmail.com>
2022-12-21 10:06:56 -0300

    test: ensure bcryptadapter returns a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -13,6 +13,12 @@ const makeSut = (): SutTypes => {
   return { sut, salt }
 }
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hashed_value'))
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
     const { sut, salt } = makeSut()
@@ -22,5 +28,13 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any_value')
 
     expect(hashSpy).toBeCalledWith('any_value', salt)
+  })
+
+  test('Should return a hash on success', async () => {
+    const { sut } = makeSut()
+
+    const value = await sut.encrypt('any_value')
+
+    expect(value).toBe('hashed_value')
   })
 })
