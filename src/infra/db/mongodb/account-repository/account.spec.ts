b15cfc526676cb3ae68f47cb9b3b80b4f7692e7b
Raphael Rego <raphael.mlrego@gmail.com>
2023-01-10 17:22:44 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,44 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+interface SutTypes {
+  sut: AccountMongoRepository
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new AccountMongoRepository()
+
+  return { sut }
+}
+
+describe('Account Mongo Repository', () => {
+  const mongoHelper = MongoHelper.getInstance()
+
+  beforeAll(async () => {
+    await mongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await mongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = mongoHelper.getCollection('accounts')
+
+    await accountCollection.deleteMany({})
+  })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const account = await sut.add({
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'hashed_password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.name).toBe('valid_name')
+    expect(account.email).toBe('valid_email@mail.com')
+    expect(account.password).toBe('hashed_password')
+  })
+})
