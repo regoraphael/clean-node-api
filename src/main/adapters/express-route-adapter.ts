912143acfafec9216a887ea41a4951b372dc3c3c
Raphael Rego <raphael.mlrego@gmail.com>
2023-01-10 20:34:05 -0300

    feat: add express route adapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,13 @@
+import { Request, RequestHandler, Response } from 'express'
+import { Controller, HttpRequest } from '../../presentation/protocols'
+
+export const adaptRoute = (controller: Controller): RequestHandler => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+
+    return res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
