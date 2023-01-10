42be84a007093ef4ffba5dc72dc7a54f23dd8f61
Raphael Rego <raphael.mlrego@gmail.com>
2023-01-10 16:42:22 -0300

    refactor: move mongodb methods to a helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,24 @@
+import { MongoClient } from 'mongodb'
+
+export class MongoHelper {
+  private static instance: MongoHelper
+  private client: MongoClient
+
+  private constructor () {}
+
+  public static getInstance (): MongoHelper {
+    if (!this.instance) {
+      this.instance = new MongoHelper()
+    }
+
+    return this.instance
+  }
+
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(uri)
+  }
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  }
+}
