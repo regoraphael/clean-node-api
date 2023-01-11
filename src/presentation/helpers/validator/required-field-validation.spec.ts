8afcd317f0aef9c5741affef52ac5e0f0771f3b0
Raphael Rego <raphael.mlrego@gmail.com>
2023-01-11 12:52:23 -0300

    test: ensure RequiredFields validations returns an error if invalid parametes is provided

---
+++ src/presentation/helpers/validator/required-field-validation.spec.ts
@@ -0,0 +1,26 @@
+import { MissingParamError } from '../../errors'
+import { RequiredFieldValidation } from './required-field-validation'
+
+interface SutTypes {
+  sut: RequiredFieldValidation
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new RequiredFieldValidation('email')
+
+  return { sut }
+}
+
+describe('RequiredField Validation', () => {
+  test('Should return an error if validation fails', () => {
+    const { sut } = makeSut()
+
+    const body = {
+      nome: 'any_name'
+    }
+
+    const error = sut.validate(body)
+
+    expect(error).toEqual(new MissingParamError('email'))
+  })
+})
