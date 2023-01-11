adb9e4b25e2b5c181dc594cb3ec6dcafcb9febeb
Raphael Rego <raphael.mlrego@gmail.com>
2023-01-11 12:45:50 -0300

    test: ensure CompareFields validations returns an error if invalid parametes is provided

---
+++ src/presentation/helpers/validator/compare-fields-validation.spec.ts
@@ -0,0 +1,27 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+interface SutTypes {
+  sut: CompareFieldsValidation
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new CompareFieldsValidation('password', 'passwordConfirmation')
+
+  return { sut }
+}
+
+describe('CompareFields Validation', () => {
+  test('Should return an error if validation fails', () => {
+    const { sut } = makeSut()
+
+    const body = {
+      password: 'password',
+      passwordConfirmation: 'other_password'
+    }
+
+    const error = sut.validate(body)
+
+    expect(error).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
+})
